e3bca8dd9d4f7f9073981e358ec79fd607ee1ad1
Екатерина Ивашкевич <katlin@Ekaterina-MacBook-Pro.local>
2021-06-14 02:42:02 +0300

    lesson28

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Todos from './components/Todos/Todos';
+import Todos from './components/Todos/TodosClass';
 
 ReactDOM.render( < Todos / > , document.getElementById('root'));
