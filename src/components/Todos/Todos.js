c01137de0373c05462e9559ab4cfa0b0b6badeaa
Екатерина Ивашкевич <katlin@Ekaterina-MacBook-Pro.local>
2021-06-15 02:19:27 +0300

    lesson_29 finish

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,25 +1,7 @@
 import './Todos.scss';
 import { useState } from "react";
-
-/*const Todos = () => {
-
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: "first todo",
-            status: "new"
-        },
-        {
-            id: 2,
-            text: "second todo",
-            status: "new"
-        },
-        {
-            id: 3,
-            text: "third todo",
-            status: "new"
-        },
-    ]);*/
+import {Button, Checkbox, FormControl, InputLabel, MenuItem, Select, TextField,} from "@material-ui/core";
+import {Edit, Delete} from "@material-ui/icons";
 
 const Todos = () => {
 
@@ -32,7 +14,7 @@ const Todos = () => {
     };
 
     const addTodo = (e) => {
-        e.preventDefault(); //чтобы форма не перегружалась
+        e.preventDefault();
         const newTodoItem = {
             id: Date.now(),
             text: newTodo,
@@ -41,35 +23,56 @@ const Todos = () => {
 
         setTodos((prevState) => [newTodoItem, ...prevState]);
         setNewTodo("");
-      //  console.log("newTodoItem", newTodoItem);
     };
+    const editTodo = id => {
 
-   /* const [tests, setTests] = useState({
-        value1: " ",
-        value2: " ",
-    });
-    const testHandler = (e) => {
-        setTests(prev => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }))
     }
-    console.log(tests);*/
 
     return (
         <div className="todos">
             <form className="todos__form" onSubmit={addTodo}>
-                <input type="text" value={newTodo} onChange={inputChange} placeholder="Your new todo..." />
-               {/*} <input type="text" value={tests.value1} name="value1" onChange={testHandler} placeholder="Your new todo2..." />
-                <input type="text" value={tests.value2} name="value2" onChange={testHandler} placeholder="Your new todo3..." />*/}
-                <button type="submit">Add todo</button>
+                <TextField  label="Your new todo..."
+                            type="text"
+                            name="todo"
+                            size={"small"}
+                            value={newTodo}
+                            onChange={inputChange}
+                            variant="outlined" />
+                <Button color="primary"
+                        type="submit"
+                        variant="contained"
+                >Add todo</Button>
+
             </form>
             <div className="todos__list">
-                {todos.length ? (todos.map(({id, text:other}) => {
+                {todos.length ? (todos.map(({id, text}) => {
+
+                    console.log(todos);
+
+                    function deleteTodo (id) {
+                        setTodos(todos.filter(todo => todo.id !== id))
+                    }
+
                     return (
                         <div className="todos__item" 
                         key={id}>
-                            {other}
+                          <p>{text}</p>
+                            <div className="todos__actions">
+                                <Button startIcon={<Edit />}
+                                        variant="contained"
+                                        color="primary"
+                                        size="small"
+                                        onClick={() => editTodo(id)}
+                                        >Edit</Button>
+                                {/* eslint-disable-next-line react/jsx-no-undef */}
+                                <Button startIcon={<Delete />}
+                                        variant="contained"
+                                        color="secondary"
+                                        size="small"
+                                        onClick={() => deleteTodo(id)}
+                                        >Delete</Button>
+
+                            </div>
                         </div>
                     );
                 })) : <h2>No todos...</h2>}
