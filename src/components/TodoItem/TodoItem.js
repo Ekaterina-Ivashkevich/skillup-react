74ee0590cb4aeeb541779bf013bf372fe5fd5ca6
Ekaterina Ivashkevich <81188812+Ekaterina-Ivashkevich@users.noreply.github.com>
2021-07-01 16:28:45 +0300

    Todo_32 (#7)

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,93 +1,111 @@
-import {Button, Checkbox, FormControl, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
-import {STATUSES} from "../../constants";
-import {Delete, Edit} from "@material-ui/icons";
-import {useState} from "react";
+import { Button, Checkbox, FormControl, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
+import { STATUSES } from "../../constants";
+import { Delete, Edit } from "@material-ui/icons";
+import { useState } from "react";
+import DialogDelete from "../DialogDelete/DialogDelete";
 
-const TodoItem = ({todo,todos,setTodos}) => {
+const TodoItem = ({ todo, todos, setTodos }) => {
+  const [isEditorActive, setIsEditorActive] = useState(false);
+  const [editingText, setEditingText] = useState("");
 
-    const[isEditorActive,setIsEditorActive] = useState(false);
-    const[editingText,setEditingText] = useState("");
-
-    function helper(id,key,value) {
-        const newTodos = todos.map(todo => {
-        if (todo.id === id) {
-       return {...todo,[key] : value}
-        }
-        return todo
-    })
-        setTodos(newTodos);
-    }
-
-    const editTodo = (id,text) => {
-        setEditingText(text);
-        setIsEditorActive(!isEditorActive);
-    if(isEditorActive){
-    helper(id,"text",editingText);
+  function helper(id, key, value) {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, [key]: value };
       }
-    }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
 
-    function statusHandler (id) {
-        helper(id, "status", todo.status !== "done" ? "done" : "new");
+  const editTodo = (id, text) => {
+    setEditingText(text);
+    setIsEditorActive(!isEditorActive);
+    if (isEditorActive) {
+      helper(id, "text", editingText);
     }
+  };
 
-    function changeStatus (e, id) {
-        helper(id,"status",e.target.value);
+  function statusHandler(id) {
+    helper(id, "status", todo.status !== "done" ? "done" : "new");
+  }
+
+  function changeStatus(e, id) {
+    helper(id, "status", e.target.value);
+  }
+
+  function deleteTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function changeTodoText(e) {
+    setEditingText(e.target.value);
+  }
+
+  function saveTodoByClickEnter(e, id, text) {
+    if (e.key === "Enter") {
+      editTodo(id, text);
     }
+  }
 
-    function deleteTodo (id) {
-        setTodos(todos.filter(todo => todo.id !== id))
-    }
+  const [isDialogDeleteOpened, setIsDialogDeleteOpened] = useState(false);
 
-    function changeTodoText (e) {
-        setEditingText(e.target.value);
-    }
-
-    function saveTodoByClickEnter (e,id,text){
-        if (e.key === "Enter"){
-            editTodo(id,text);
-        }
-    }
-
-    return (
-        <div className="todos__item"
-             key={todo.id}>
-            <Checkbox
-                color ="default"
-                checked={todo.status === "done"}
-                onChange={() => statusHandler(todo.id)}
-            />
-            {isEditorActive ? (<TextField  autoFocus={true}
-                                           onKeyUp={(e) => saveTodoByClickEnter(e,todo.id,todo.text)}
-                                           onChange={changeTodoText}
-                                           value={editingText}/>) :
-                <p className="todos__text">{todo.text}</p>}
-            <div className="todos__actions">
-                <FormControl className="todos__select">
-                    <InputLabel>Status</InputLabel>
-                    <Select value={todo.status}
-                            onChange={function (e) {
-                                return changeStatus(e, todo.id)}}>
-                        {STATUSES.map((status) => (
-                            <MenuItem value={status}
-                                      key={status}>{status}</MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <Button startIcon={<Edit />}
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        onClick={() => editTodo(todo.id,todo.text)}
-                >Edit</Button>
-                <Button startIcon={<Delete />}
-                        variant="contained"
-                        color="secondary"
-                        size="small"
-                        onClick={() => deleteTodo(todo.id)}
-                >Delete</Button>
-            </div>
-        </div>
-    );
+  return (
+    <>
+      <div className="todos__item" key={todo.id}>
+      <Checkbox color="default" checked={todo.status === "done"} onChange={() => statusHandler(todo.id)} />
+      {isEditorActive ? (
+          <TextField
+              autoFocus={true}
+              onKeyUp={(e) => saveTodoByClickEnter(e, todo.id, todo.text)}
+              onChange={changeTodoText}
+              value={editingText}
+          />
+      ) : (
+          <p className="todos__text">{todo.text}</p>
+      )}
+      <div className="todos__actions">
+        <FormControl className="todos__select">
+          <InputLabel>Status</InputLabel>
+          <Select
+              value={todo.status}
+              onChange={function (e) {
+                return changeStatus(e, todo.id);
+              }}
+          >
+            {STATUSES.map((status) => (
+                <MenuItem value={status} key={status}>
+                  {status}
+                </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button
+            startIcon={<Edit />}
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={() => editTodo(todo.id, todo.text)}
+        >
+          Edit
+        </Button>
+        <Button
+            startIcon={<Delete />}
+            variant="contained"
+            color="secondary"
+            size="small"
+            //onClick={() => deleteTodo(todo.id)}
+            onClick={()=> setIsDialogDeleteOpened(true)}
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+      <DialogDelete isDialogDeleteOpened={isDialogDeleteOpened}
+                    setIsDialogDeleteOpened={setIsDialogDeleteOpened}
+                    deleteFunction={() =>deleteTodo(todo.id)}/>
+    </>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,79 +1,101 @@
-import './Todos.scss';
-import { useState,  useEffect} from "react";
-import {Button, Checkbox, TextField,FormControlLabel} from "@material-ui/core";
-import TodoItem  from "../TodoItem/TodoItem";
+import "./Todos.scss";
+import { useState, useEffect } from "react";
+import { Button, TextField, FormControlLabel } from "@material-ui/core";
+import TodoItem from "../TodoItem/TodoItem";
 import IOSSwitch from "../IOSSwitch/IOSSwitch";
+import { STATUSES } from "../../constants/index";
+import Logger from "../../constants/utils/Logger";
 
 const Todos = () => {
-    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
-    const [newTodo, setNewTodo] = useState("");
-    const [switchTodo,setSwitchTodo] = useState (false);
-    const inputChange = (e) => {
-        setNewTodo(e.target.value);
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
+  const [newTodo, setNewTodo] = useState("");
+  const [switchTodo, setSwitchTodo] = useState(false);
+
+  const inputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const addTodo = (e) => {
+    e.preventDefault();
+    const newTodoItem = {
+      id: Date.now(),
+      text: newTodo,
+      status: "new",
     };
-    const addTodo = (e) => {
-        e.preventDefault();
-        const newTodoItem = {
-            id: Date.now(),
-            text: newTodo,
-            status: "new"
-        }
-        setTodos((prevState) => [newTodoItem, ...prevState]);
-        setNewTodo("");
-    };
-    console.log(todos);
+    setTodos((prevState) => [newTodoItem, ...prevState]);
+    setNewTodo("");
+  };
 
-    function saveTodos () {
-        localStorage.setItem('todos', JSON.stringify(todos))
+  function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const switchHandler = ({ target: { checked } }) => {
+    setSwitchTodo(checked);
+  };
+
+  useEffect(() => {
+    if (switchTodo) {
+      saveTodos();
     }
+  }, [todos]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const switchOnOff = ({target: {checked}})=>{
-        setSwitchTodo(checked);
-    }
+  const [currentStatus, setCurrentStatus] = useState("new");
 
-    useEffect(()=>{
-        if(switchTodo){
-        saveTodos();}},
-        [todos])
+  function tabHandler(status) {
+    setCurrentStatus(status);
+    // про синхронность setState
+    // Logger.info("tabHandler", currentStatus);
+  }
 
-    return (
-        <div className="todos">
-            <form className="todos__form" onSubmit={addTodo}>
-                <TextField  label="Your new todo..."
-                            type="text"
-                            name="todo"
-                            size={"small"}
-                            value={newTodo}
-                            onChange={inputChange}
-                            variant="outlined" />
-                <Button color="primary"
-                        type="submit"
-                        variant="contained"
-                >Add todo</Button>
-            </form>
-            {todos.length ? <Button variant="outlined"
-                                    color="primary"
-                                    className="todos__save"
-                                    onClick={saveTodos}>Save todos</Button> : null}
-            <div className="todos__autosave">
+  useEffect(() => {
+    //Logger.info("useEffect",currentStatus);
+  }, [currentStatus]);
 
-                <FormControlLabel
-                    control={<IOSSwitch checked={switchTodo}
-                                        onChange={switchOnOff} />}
-                    label="Autosave"
-                />
-                <Checkbox
-                    color="default"
-                    checked={switchTodo}
-                    onChange={switchOnOff}
-                />
-                Autosave
-            </div>
-            <div className="todos__list">
-                {todos.length ? (todos.map((todo) => <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>)) : <h2>No todos...</h2>}
-            </div>
-        </div>
-    );
+  return (
+    <div className="todos">
+      <form className="todos__form" onSubmit={addTodo}>
+        <TextField
+          label="Your new todo..."
+          type="text"
+          name="todo"
+          size={"small"}
+          value={newTodo}
+          onChange={inputChange}
+          variant="outlined"
+        />
+        <Button color="primary" type="submit" variant="contained">
+          Add todo
+        </Button>
+      </form>
+      {todos.length ? (
+        <Button variant="outlined" color="primary" className="todos__save" onClick={saveTodos}>
+          Save todos
+        </Button>
+      ) : null}
+      <div className="todos__autosave">
+        <FormControlLabel control={<IOSSwitch checked={switchTodo} onChange={switchHandler} />} label="Autosave" />
+      </div>
+      <div className="todos__filter">
+        {STATUSES.map((status) => (
+          <Button key={status}
+                  variant={status === currentStatus ? "contained" : "outlined"}
+                  color="primary"
+                  onClick={() => tabHandler(status)}>
+            {status}
+          </Button>
+        ))}
+      </div>
+      <div className="todos__list">
+        {todos.length ? (
+            todos.
+            filter((todo) => todo.status === currentStatus)
+          .map((todo) => <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
+        ) : (
+          <h2>No todos...</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Todos;
